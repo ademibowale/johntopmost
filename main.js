d9573fd8b7af62d659133766a95a09049f0d34dc
ademibowale <ademibowaleadegboye@gmail.com>
2023-11-06 19:31:21 +0100

    implement the javascript slide

--- main.js
+++ main.js
@@ -61,3 +61,22 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-pagination",
   },
 });
+
+let slideIndex = 0;
+function showSlides() {
+  let i;
+  const slides = document.getElementsByClassName('mySlides');
+  const dots = document.getElementsByClassName('dot');
+  for (i = 0; i < slides.length; i += 1) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex += 1;
+  if (slideIndex > slides.length) { slideIndex = 1; }
+  for (i = 0; i < dots.length; i += 1) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+showSlides();
